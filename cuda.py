9f0408a80c462c8e2a8d9bc981c87212d090345d
ZeroFindFire <1174534295@qq.com>
2018-07-10 21:51:54 +0800

    update

--- cuda.py
+++ cuda.py
@@ -23,15 +23,62 @@ source = """
 """
 mod = SourceModule(source)
 test = mod.get_function("test")
-a = np.arange(100,dtype = np.float64)
-b = np.arange(100,dtype = np.float64)
+size = 100
+a = np.arange(size,dtype = np.float64)
+b = np.arange(size,dtype = np.float64)
 outs = drv.InOut(a)
 ins = drv.In(b)
-sizes = drv.In(np.array([100],dtype=np.int32))
-test(outs,ins,sizes,block = (100,1,1),grid=(1,1))
-print "resule:",a
+sizes = drv.In(np.array([size],dtype=np.int32))
+test(outs,ins,sizes,block = (size,1,1),grid=(1,1))
+print("resule:",a.shape)
 
+def img2arr(img):
+	img = np.array(img).astype(np.float)
+	img /= 256
+	shape = list(img.shape)
+	if len(shape)==2:
+		img.shape = [1]+shape
+		return img 
+	elif len(shape) != 3:
+		raise Exception("Unknow shape of image: "+str(shape))
+	depth = shape[2]
+	shape = [depth] + shape[:-1]
+	outs = np.zeros(shape,dtype = img.dtype)
+	for i in xrange(depth):
+		outs[i,:,:]=img[:,:,i]
+	return outs 
 
+def arr2img(img):
+	img = np.array(img)
+	img *= 256
+	img = img.astype(np.uint8)
+	shape = list(img.shape)
+	if len(shape)==2:
+		return img
+	elif len(shape) != 3:
+		raise Exception("Unknow shape of array: "+str(shape))
+	depth = shape[0]
+	shape = shape[1:]+[depth]
+	outs = np.zeros(shape,dtype = img.dtype)
+	for i in xrange(depth):
+		outs[:,:,i]=img[i,:,:]
+	if outs.shape[-1] == 1:
+		outs.shape = outs.shape[:-1]
+	return outs
+
+def show(img, tmp_path = r"E:\tmp.bmp",wait_time = 3.0, clean = False):
+	from PIL import Image
+	img=Image.fromarray(img)
+	img.save(tmp_path)
+	import os 
+	os.system(r"start %s"%(tmp_path,))
+	if not clean:
+		return 
+	import time 
+	time.sleep(wait_time)
+	del_cmd = r"del /F /Q %s"%(tmp_path,)
+	os.system(del_cmd)
+	
 
 
 filter_source = """
@@ -57,7 +104,8 @@ filter_source = """
 	#define step_width_image parameters[14]
 	#define step_thread parameters[15]
 	#define max_num_thread parameters[16]
-	__device__ Gfloat piece_filter(const Gfloat* image, const Gfloat* filter, const int* parameters, const int base_left, const int base_top){
+	__device__ Gfloat piece_filter(const Gfloat* image, const Gfloat* filter,
+		const int* parameters, const int base_left, const int base_top){
 		Gfloat rst = 0;
 		int index_height_image = base_top;
 		int index_width_image = base_left;
@@ -72,7 +120,7 @@ filter_source = """
 					continue;
 				}
 				rst += image[index_height_image * width_image + index_width_image] 
-					* filter[index_height_filter * filter_width + index_width_filter];
+					* filter[index_height_filter * width_filter + index_width_filter];
 			}
 			index_width_image = base_left;
 		}
@@ -107,7 +155,7 @@ filter_source = """
 			index_thread += step_thread;
 		}
 	}
-	__device__ void filter_point_feedback(const Gfloat* images, const Gfloat* alters, const int* parameters, 
+	__device__ void filter_point_feedback(const Gfloat* images, Gfloat* alters, const int* parameters, 
 		const Gfloat* reverses, int index_thread){
 		int index_width_filter = index_thread % width_filter;
 		index_thread /= width_filter;
@@ -133,7 +181,7 @@ filter_source = """
 					if (index_width_image < 0 || index_width_image >= width_image) {
 						continue;
 					}
-					rst += images[index_height_image * width_image + index_width_image] * reverse[index_top * num_left + index_left];
+					rst += images[index_height_image * width_image + index_width_image] * reverses[index_top * num_left + index_left];
 				}
 			}
 			reverses += num_left * num_top * nums_filter;
@@ -213,20 +261,28 @@ Same=1
 Full=2
 ft_mod = None#SourceModule(filter_source)
 g_filter = None#ft_mod.get_function("g_filter")
-def image_filter(image, filter):
+def image_filter(image, filter, nums_thread = 100):
+	global ft_mod 
+	global g_filter 
+	if ft_mod is None:
+		ft_mod = SourceModule(filter_source)
+		g_filter = ft_mod.get_function("g_filter")
 	image = np.array(image).astype(np.float64)
 	filter = np.array(filter).astype(np.float64)
-	image = image.reshape([1]+list(image.shape))
-	filter = filter.reshape([1]+list(filter.shape))
-	parameters, outshape = build_parameters(image, filters, 100)
+	if len(image.shape) != 4:
+		image = image.reshape([1]+list(image.shape))
+	if len(filter.shape) != 4:
+		filter = filter.reshape([1]+list(filter.shape))
+	parameters, outshape = build_parameters(image, filter, nums_thread)
+	#print("outshape:",outshape,"from shape", image.shape)
 	outputs = np.zeros(outshape,dtype = np.float64)
-	g_filter(drv.In(image),drv.In(filter),drv.In(parameters),drv.Out(outputs),block = (100,1,1),grid=(1,1))
+	g_filter(drv.In(image),drv.In(filter),drv.In(parameters),drv.Out(outputs),block = (nums_thread,1,1),grid=(1,1))
 	return outputs
-def build_parameters(images, filters, num_threads, step_left=1, step_top=1, step_width_image=1, step_height_image=1, filter_type = Valid):
+def build_parameters(images, filters, nums_thread, step_left=1, step_top=1, step_width_image=1, step_height_image=1, filter_type = Valid):
 	nums_image, depth, height_image, width_image = images.shape 
 	nums_filter, depth, height_filter, width_filter = filters.shape
-	height_box = height_filter + (height_filter - 1) * step_height_image;
-	width_box = width_filter + (width_filter - 1) * step_width_image;
+	height_box = 1 + (height_filter - 1) * step_height_image;
+	width_box = 1 + (width_filter - 1) * step_width_image;
 	if filter_type == Valid:
 		left, top = 0, 0
 		#left + last_index_left * step_left + width_box <= width_image
@@ -251,7 +307,7 @@ def build_parameters(images, filters, num_threads, step_left=1, step_top=1, step
 	rst += [left, top, step_left, step_top, num_left, num_top, step_height_image, step_width_image]
 	total_num = nums_image * nums_filter * num_left * num_top
 	max_num_thread = total_num
-	step_thread = min(num_threads,total_num)
+	step_thread = min(nums_thread,total_num)
 	rst += [step_thread, max_num_thread]
 	rst = np.array(rst,dtype = np.int32)
 	return rst,[nums_image,nums_filter,num_top,num_left]
